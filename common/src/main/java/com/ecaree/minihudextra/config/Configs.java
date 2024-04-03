c4e947cfa54971a3dd0b56266c2bcc4f9e6f528b
Ecaree <2551728057@qq.com>
2024-04-03 10:02:28 +0800

    🎨 Format

--- common/src/main/java/com/ecaree/minihudextra/config/Configs.java
+++ common/src/main/java/com/ecaree/minihudextra/config/Configs.java
@@ -91,7 +91,9 @@ public class Configs implements IConfigHandler {
                 LINE_FORTYONE, LINE_FORTYTWO, LINE_FORTYTHREE, LINE_FORTYFOUR, LINE_FORTYFIVE, LINE_FORTYSIX, LINE_FORTYSEVEN, LINE_FORTYEIGHT, LINE_FORTYNINE, LINE_FIFTY
         );
     }
-    public static void loadFromFile() {
+
+    @Override
+    public void load() {
         File configFile = new File(FileUtils.getConfigDirectory(), CONFIG_FILE_NAME);
         if (configFile.exists() && configFile.isFile() && configFile.canRead()) {
             JsonElement element = JsonUtils.parseJsonFile(configFile);
@@ -102,7 +104,8 @@ public class Configs implements IConfigHandler {
             }
         }
     }
-    public static void saveToFile() {
+    @Override
+    public void save() {
         File dir = FileUtils.getConfigDirectory();
         if (dir.exists() && dir.isDirectory() || dir.mkdirs()) {
             JsonObject root = new JsonObject();
@@ -110,14 +113,5 @@ public class Configs implements IConfigHandler {
             ConfigUtils.writeConfigBase(root, "Generic", Configs.Generic.OPTIONS);
             JsonUtils.writeJsonToFile(root, new File(dir, CONFIG_FILE_NAME));
         }
-    }
-
-    @Override
-    public void load() {
-        loadFromFile();
-    }
-    @Override
-    public void save() {
-        saveToFile();
     }
 }
