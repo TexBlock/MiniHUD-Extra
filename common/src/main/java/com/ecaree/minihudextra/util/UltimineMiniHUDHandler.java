d82d6d426a1cefbdb23e7efa9785813f54962bcd
Ecaree <2551728057@qq.com>
2024-04-13 19:09:55 +0800

    🐛 Deprecate buggy packet receiver

---
+++ common/src/main/java/com/ecaree/minihudextra/util/UltimineMiniHUDHandler.java
@@ -0,0 +1,34 @@
+package com.ecaree.minihudextra.util;
+
+import dev.architectury.event.EventResult;
+import dev.architectury.platform.Platform;
+import dev.ftb.mods.ftbultimine.client.FTBUltimineClient;
+import fi.dy.masa.minihud.config.Configs;
+import net.minecraft.client.MinecraftClient;
+
+public class UltimineMiniHUDHandler {
+    private static boolean wasEnabledInitially = false;
+
+    public static void interactionEvents() {
+        if (!Platform.isModLoaded("ftbultimine")) return;
+
+        if (FTBUltimineClient.keyBinding.wasPressed()) {
+            if (!wasEnabledInitially) {
+                wasEnabledInitially = Configs.Generic.MAIN_RENDERING_TOGGLE.getBooleanValue();
+                Configs.Generic.MAIN_RENDERING_TOGGLE.setBooleanValue(false);
+            }
+        } else {
+            if (wasEnabledInitially) {
+                Configs.Generic.MAIN_RENDERING_TOGGLE.setBooleanValue(true);
+                wasEnabledInitially = false;
+            }
+        }
+    }
+
+    public static EventResult onKeyPress(MinecraftClient client, int keyCode, int scanCode, int action, int modifiers) {
+        if (com.ecaree.minihudextra.config.Configs.Generic.FTB_ULTIMINE_SUPPORT.getBooleanValue()) {
+            interactionEvents();
+        }
+        return EventResult.pass();
+    }
+}
