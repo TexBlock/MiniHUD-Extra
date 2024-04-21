b463d2c2c15eed4729326087dbf9d8f679f7ed39
Ecaree <2551728057@qq.com>
2024-04-21 13:56:24 +0800

    🐛 Fix color

--- forge/src/main/java/com/ecaree/minihudextra/integration/forge/MekRadiationImpl.java
+++ forge/src/main/java/com/ecaree/minihudextra/integration/forge/MekRadiationImpl.java
@@ -4,11 +4,14 @@ import mekanism.api.MekanismAPI;
 import mekanism.common.lib.radiation.RadiationManager;
 import mekanism.common.util.UnitDisplayUtils;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Formatting;
 
 public class MekRadiationImpl {
     public static String getRadiationString(PlayerEntity player) {
         double magnitude = MekanismAPI.getRadiationManager().getRadiationLevel(player);
         String colorCode = RadiationManager.RadiationScale.getSeverityColor(magnitude).code;
-        return colorCode + UnitDisplayUtils.getDisplayShort(magnitude, UnitDisplayUtils.RadiationUnit.SVH, 3).asString();
+        return colorCode
+                + UnitDisplayUtils.getDisplayShort(magnitude, UnitDisplayUtils.RadiationUnit.SVH, 3).asString()
+                + Formatting.RESET;
     }
 }
